0cfd04f0d6f5f0d49d89d17bd252673f6dc3fe67
NITRO 5 <Peerapatpee@hotmail.com>
2020-08-27 00:28:40 +0700

    Setting Gradle create  and setting ContactFragment.kt create  and setting ProfileFragment.kt create navigation.xml create nav_menu.xml setting activity_main.xml

---
+++ app/src/main/java/peerapat/yodnil/newfirstapp/ProfileFragment.kt
@@ -0,0 +1,41 @@
+package peerapat.yodnil.newfirstapp
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import peerapat.yodnil.newfirstapp.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var binding = DataBindingUtil.inflate<FragmentProfileBinding>(
+            inflater,
+            R.layout.fragment_profile,
+            container,
+            false
+        )
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.nav_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
+
+}
